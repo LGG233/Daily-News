9fecba71d2ec07bd5c72f41df1f9058f8ce3fb70
Fred Astaire <lancebd@thegodardgroup.com>
2019-03-13 01:32:15 -0400

    Two big changes - 'most viewed' at startup and revamped style

--- assets/javascript/newsreader.js
+++ assets/javascript/newsreader.js
@@ -13,12 +13,13 @@ $(document).ready(function () {
     addSearchButton();
     frenchify();
     englishify();
+    engStartup();
 });
 
 function frenchify() {
     $("#main-title").text("Toutes Les Nouvelles Dont Vous Avez Besoin");
     $("#reading").html("<br><br>Ces articles viennent de <em>Libération</em>. Abonnement obligatoire");
-    $("#lang-lead").html("Cliquez un sujet pour voir jusqu'à dix articles de <em>Libération</em> à ce sujet");
+    // $("#lang-lead").html("Cliquez un sujet pour voir jusqu'à dix articles de <em>Libération</em> à ce sujet");
     $("#search-input").html("<span id='search-input'><strong>Que désirez-vous voir de plus?</strong><input id='searchText' type='text'>");
     $("#add-button").html("On y va !");
     $("#articles-space").empty();
@@ -27,14 +28,13 @@ function frenchify() {
 
 function englishify() {
     $("#main-title").text("All The News That's Fit To Read");
-    $("#reading").html("<br><br>Articles come from <em>The New York Times</em>. Subscription required");
-    $("#lang-lead").html("Click a button to get up to ten articles on that topic from the <em>New York Times</em>");
-    $("#search-input").html("<span id='search-input'><strong>What else do you want to read?</strong><input id='searchText' type='text'>");
-    $("#add-button").html("Let's go!");
+    $("#reading").html("All articles come from <em>The New York Times</em>. Subscription required");
+    // $("#lang-lead").html("Click a button to get up to ten articles on that topic from the <em>New York Times</em>");
+    $("#search-input").html("<span id='search-input'><strong>New search</strong><input id='searchText' size='15' type='text'>");
+    $("#add-button").html("<button class='btn btn-primary' id='add-button' type='submit'>Add search");
     $("#articles-space").empty();
     $("#articles-title").empty();
 }
-
 function renderButtons() {
     $("#button-space").empty();
     if (language === "English") {
@@ -44,7 +44,10 @@ function renderButtons() {
     }
     for (var i = 0; i < database.length; i++) {
         var a = $("<button>");
-        a.addClass("button-text", "btn-info");
+        a.addClass("btn");
+        a.addClass("btn-primary");
+        a.addClass("search-button");
+        a.attr("type", "button");
         a.attr("data-name", database[i]);
         a.text(database[i]);
         $("#button-space").append(a);
@@ -85,21 +88,35 @@ function langChooser() {
 
 function getArticles() {
     console.log("getArticles is running");
-    $(".button-text").on("click", function () {
-        $("#articles-space").empty();
+    $(".search-button").on("click", function () {
         articleSearch = $(this).attr("data-name");
         if (language === "English") {
             queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + articleSearch + "&page=0&sort=newest&api-key=hh8LJpb49GiBE4VMM6TKst92CHnrv9cy"
-            // $("#articles-title").html("<h3>Here are your articles on " + articleSearch + ". Enjoy!</h3>");
             engArticleSearch();
         } else {
             queryURL = "https://newsapi.org/v2/everything?q=" + articleSearch + "&sources=liberation&sortBy=popularity&apiKey=b39076bb4e5d4f61a4974e9c2ab2e755";
-            // $("#articles-title").html("<h3>Voici vos articles au sujet de " + articleSearch + ". Bonne lecture !</h3>");
             frenArticleSearch();
         }
     })
 };
 
+function engStartup() {
+    var queryURL = "https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=hh8LJpb49GiBE4VMM6TKst92CHnrv9cy";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+            for (var j = 0; j < 10; j++) {
+                var headline = response.results[j].title;
+                var url = response.results[j].url;
+                var author = response.results[j].byline;
+                var content = response.results[j].abstract;
+                renderHTML(url, headline, author, content);
+            };
+        })
+}
 function engArticleSearch() {
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + articleSearch + "&page=0&sort=newest&api-key=hh8LJpb49GiBE4VMM6TKst92CHnrv9cy";
     $.ajax({
@@ -108,7 +125,8 @@ function engArticleSearch() {
     })
         .then(function (response) {
             console.log(response);
-            // $("#articles-title").html("<h3>Here are your articles on " + articleSearch + ". Enjoy!</h3>");
+            console.log("let's clear out the articles-space")
+            $("#articles-space").empty();
             for (var j = 0; j < 10; j++) {
                 var headline = response.response.docs[j].headline.main;
                 var url = response.response.docs[j].web_url;
@@ -127,8 +145,7 @@ function frenArticleSearch() {
     })
         .then(function (response) {
             console.log(response);
-            // $("#articles-space").empty();
-            // $("#articles-title").html("<h3>Voici vos articles au sujet de " + articleSearch + ". Bonne lecture !</h3>");
+            $("#articles-space").empty();
             for (var j = 0; j < 10; j++) {
                 var headline = response.articles[j].title;
                 var url = response.articles[j].url;
